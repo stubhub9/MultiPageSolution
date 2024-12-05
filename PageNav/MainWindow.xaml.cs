060c4a9c8d7377ac4a4a680b83892b1d41d0076e
Stu Bilyeu <stublue1@outlook.com>
2024-12-05 15:09:42 -0800

    Add project files.

---
+++ PageNav/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PageNav
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Page1 ( object sender, RoutedEventArgs e )
+        {
+            MainFrame.Content = new Page1 ();
+        }
+
+        private void Button_Click_Page2 ( object sender, RoutedEventArgs e )
+        {
+            MainFrame.Content = new Page2 ();
+
+        }
+    }
+}
